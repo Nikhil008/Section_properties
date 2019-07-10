7f98de07fb72258d308c644aedbdd4a229ff7dc1
Nikhil <nikhilkaps@gmail.com>
2019-07-10 05:58:55 -0700

    ISMB section

--- section_properties.py
+++ section_properties.py
@@ -238,11 +238,30 @@ class Sector(Section):
         self.I_x = self.Radius**4 / 8 * (self.Angle - math.sin(self.Angle))
         return self.I_x
 
+    def r_x(self):
+        self.R_x = math.sqrt(self.i_x() / self.area())
+        return self.R_x
 
-class Fillet(Sector, Triangle):
+    def r_y(self):
+        self.R_y = math.sqrt(self.i_y() / self.area())
+        return self.R_y
+
+    def ze_x(self):
+        return self.Ze_x
+
+    def ze_y(self):
+        return self.Ze_y
+
+    def zp_x(self):
+        return self.Zp_x
+
+    def zp_y(self):
+        return self.Zp_y
+
+class Fillet(Section):
 
     def __init__(self, radius, angle):
-        Section.__init__()  # How to use super here?
+        super(Fillet, self).__init__()  # How to use super here?
         self.Radius = radius
         self.Angle = angle
         self.Fillet_Sector = Sector(self.Radius, math.pi - self.Angle)
@@ -275,7 +294,7 @@ class Fillet(Sector, Triangle):
         i_y_triangles_axis = 2 * parallel_axis_transform_i(self.Fillet_Triangle.i_y(), self.Fillet_Triangle.area(),
                                                       self.Fillet_Triangle.centroid()[0])
         i_y_sector_axis = parallel_axis_transform_i(self.Fillet_Sector.i_y(), self.Fillet_Sector.area(),
-                                                    self.Fillet_Triangle.Base - self.Fillet_Sector.centroid())
+                                                    self.Fillet_Triangle.Base - self.Fillet_Sector.centroid()[0])
         i_y_axis = i_y_triangles_axis - i_y_sector_axis
         self.I_y = parallel_axis_transform_i(i_y_axis, -self.area(), self.centroid()[0])#note: negative area is imp
         return self.I_y
@@ -288,6 +307,20 @@ class Fillet(Sector, Triangle):
         self.R_y = math.sqrt(self.i_y()/self.area())
         return self.R_y
 
+    def ze_x(self):
+        return self.Ze_x
+
+    def ze_y(self):
+        return self.Ze_y
+
+    def zp_x(self):
+        return self.Zp_x
+
+    def zp_y(self):
+        return self.Zp_y
+
+
+    """
     def ze_x(self):
         self.Ze_x = 2 * self.i_x() / self.Breadth
         return self.Ze_x
@@ -302,4 +335,104 @@ class Fillet(Sector, Triangle):
 
     def zp_y(self):
         self.Zp_y = self.Breadth * self.Length ** 2 / 4
+        return self.Zp_y
+    """
+
+
+class ISMB_section(Section):
+
+    def __init__(self, d, b, tf, tw, r1, r2, angle):
+        super(ISMB_section, self).__init__()  # How to use super here?
+        self.D = d
+        self.B = b
+        self.T = tf
+        self.t = tw
+        self.R1 = r1
+        self.R2 = r2
+        self.alpha = angle
+        self.Fillet_1 = Fillet(self.R1, self.alpha)
+        self.Fillet_2 = Fillet(self.R2, self.alpha)
+        h = (self.B - self.t)/2/math.tan(math.pi - self.alpha)
+        self.Rectangle_1 = Rectangle(self.t, self.D - 2*self.T - h)
+        self.Rectangle_2 = Rectangle(self.B, self.T - h/2)
+        self.Triangle_1 = Triangle((self.B - self.t)/2, self.T + h/2, math.pi/2)
+
+    def __repr__(self):
+        repr = "ISMB\n"
+        repr += super(ISMB_section, self).__repr__()  # How to use super here?
+        return repr
+
+    def centroid(self):
+        self.Centroid = (0, 0)
+        return self.Centroid
+
+    def area(self):
+        self.Area = self.Rectangle_1.area() + 2 * self.Rectangle_2.area() + 4 * self.Triangle_1.area() + 4 * \
+                    self.Fillet_1.area() - 4 * self.Fillet_2.area()
+        return self.Area
+
+    def i_x(self):
+        h = (self.B - self.t) / 2 / math.tan(math.pi - self.alpha)
+        L1 = self.D/2 - (self.T + h/2)
+        L2 = self.D/2 - (self.T - h/2)
+        I_rectangles = self.Rectangle_1.i_x() + 2 * parallel_axis_transform_i(self.Rectangle_2.i_x(),
+                                                                          self.Rectangle_2.area(), self.D/2 -
+                                                                          self.Rectangle_2.Breadth/2)
+        I_triangles = 4 * parallel_axis_transform_i(self.Triangle_1.i_x(), self.Triangle_1.area(), self.D/2 -
+                                                    self.Rectangle_1.Breadth - self.Triangle_1.centroid()[1])
+        I_fillet1_x = parallel_axis_transform_i(self.Fillet_1.i_x(), self.Fillet_1.area(), L1 * math.sin(self.alpha/2)
+                                                + self.t/2 * math.cos(self.alpha/2))
+        I_fillet2_x = parallel_axis_transform_i(self.Fillet_2.i_x(), self.Fillet_2.area(), self.B/2
+                                                * math.cos(self.alpha/2) + L2 * math.sin(self.alpha/2))
+        I_fillet1_y = parallel_axis_transform_i(self.Fillet_1.i_y(), self.Fillet_1.area(),
+                                                L1*math.cos(self.alpha/2) - self.t/2 * math.sin(self.alpha/2)
+                                                - self.Fillet_1.centroid()[0])
+        I_fillet2_y = parallel_axis_transform_i(self.Fillet_2.i_y(), self.Fillet_2.area(),
+                                                self.B/2 * math.sin(self.alpha/2) - L2 * math.cos(self.alpha/2) -
+                                                self.Fillet_2.centroid()[0])
+        I_fillets_x = 4 * rotational_transform_i(I_fillet1_x, I_fillet1_y, 0, math.pi/2 - self.alpha/2)[0] - \
+                      4 * rotational_transform_i(I_fillet2_x, I_fillet2_y, 0, math.pi/2 - self.alpha/2)[0]
+        self.I_x = I_rectangles + I_triangles + I_fillets_x
+        return self.I_x
+
+    def i_y(self):
+        h = (self.B - self.t) / 2 / math.tan(math.pi - self.alpha)
+        L1 = self.D / 2 - (self.T + h / 2)
+        L2 = self.D / 2 - (self.T - h / 2)
+        I_rectangles = self.Rectangle_1.i_y() + 2 * self.Rectangle_2.i_y()
+        I_triangles = 4 * parallel_axis_transform_i(self.Triangle_1.i_y(), self.Triangle_1.area(), self.t / 2 +
+                                                    self.Triangle_1.centroid()[0])
+        I_fillet1_x = parallel_axis_transform_i(self.Fillet_1.i_x(), self.Fillet_1.area(), L1 * math.sin(self.alpha / 2)
+                                                + self.t / 2 * math.cos(self.alpha / 2))
+        I_fillet2_x = parallel_axis_transform_i(self.Fillet_2.i_x(), self.Fillet_2.area(), self.B / 2
+                                                * math.cos(self.alpha / 2) + L2 * math.sin(self.alpha / 2))
+        I_fillet1_y = parallel_axis_transform_i(self.Fillet_1.i_y(), self.Fillet_1.area(),
+                                                L1 * math.cos(self.alpha / 2) - self.t / 2 * math.sin(self.alpha / 2)
+                                                - self.Fillet_1.centroid()[0])
+        I_fillet2_y = parallel_axis_transform_i(self.Fillet_2.i_y(), self.Fillet_2.area(),
+                                                self.B / 2 * math.sin(self.alpha / 2) - L2 * math.cos(self.alpha / 2) -
+                                                self.Fillet_2.centroid()[0])
+        I_fillets_y = 4 * rotational_transform_i(I_fillet1_x, I_fillet1_y, 0, math.pi / 2 - self.alpha / 2)[1] - \
+                      4 * rotational_transform_i(I_fillet2_x, I_fillet2_y, 0, math.pi / 2 - self.alpha / 2)[1]
+        self.I_y = I_rectangles + I_triangles + I_fillets_y
+        return self.I_y
+
+    def r_x(self):
+        self.R_x = math.sqrt(self.i_x()/self.area())
+        return self.R_x
+
+    def r_y(self):
+        self.R_y = math.sqrt(self.i_y()/self.area())
+        return self.R_y
+
+    def ze_x(self):
+        return self.Ze_x
+
+    def ze_y(self):
+        return self.Ze_y
+
+    def zp_x(self):
+        return self.Zp_x
+
+    def zp_y(self):
         return self.Zp_y
